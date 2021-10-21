03bbb2f2e6aa0fd71a31d53c52359ce1eca292f8
ganesh-rc <ganesh.rc@surya-soft.com>
2021-10-21 17:45:51 +0530

    added show functionality

--- client1/src/components/Header.js
+++ client1/src/components/Header.js
@@ -9,7 +9,6 @@ const Header = () => {
             <div className='right menu'>
                 <Link to='/' className='item'> All streams</Link>
                 <Link to='/streams/new' className='item'> create stream</Link>
-                <Link to='/streams/show' className='item'>show stream </Link>
 
                 <GoogleAuth />
             </div>
--- client1/src/components/Modal.js
+++ client1/src/components/Modal.js
@@ -18,4 +18,5 @@ const Modal = props => {
   };
   
   export default Modal;
+
   
